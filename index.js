442d6f764194d013c0224cad7031bb4b0d7dc8ae
Martina Kurkova <martinanoha@seznam.cz>
2025-04-20 10:41:57 +0200

    hotovo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('funguju!');
+const updateSkill = (id, value) => {
+    const skillElement = document.getElementById(id);
+    const valueElement = skillElement.querySelector('.skill__value');
+    const progressElement = skillElement.querySelector('.skill__progress');
+
+    valueElement.textContent = `${value}/100`;
+    progressElement.style.width = `${value}%`;
+};
+
+updateSkill("skill1", Number(prompt("Zadejte číslem od 1 do 100 úroveň svých znalostí HTML:")));
+updateSkill("skill2", Number(prompt("Zadejte číslem od 1 do 100 úroveň svých znalostí CSS:")));
+updateSkill("skill3", Number(prompt("Zadejte číslem od 1 do 100 úroveň svých znalostí JS:")));
+
